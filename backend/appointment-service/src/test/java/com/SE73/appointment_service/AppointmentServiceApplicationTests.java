239fe831f19efe1b02b5aca985acc995d0098eb4
Pavan Uthsara <uthsarapavan@gmail.com>
2026-04-06 23:14:34 +0530

    initialize appointment service

---
+++ backend/appointment-service/src/test/java/com/SE73/appointment_service/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SE73.appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
