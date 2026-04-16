c8e4afa9349c1985d4c86fab8e4b4017bfc6b995
kaveeshapasan2002 <it23292604@my.sliit.lk>
2026-04-16 22:37:26 +0530

    Add StripeConfig for Stripe API key configuration

---
+++ backend/payment-service/src/main/java/com/se73/payment_service/config/StripeConfig.java
@@ -0,0 +1,13 @@
+package com.se73.payment_service.config;
+
+import com.stripe.Stripe;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StripeConfig {
+
+    public StripeConfig(@Value("${stripe.api-key}") String stripeApiKey) {
+        Stripe.apiKey = stripeApiKey;
+    }
+}
